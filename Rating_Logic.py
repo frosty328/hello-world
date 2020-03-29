5bc6c02e84221c42cf01a0ee25328baabaefd2b1
Alan Frost <alan.f328@outlook.com>
2020-03-29 14:48:22 +0100

    .gitignore fix

---
+++ Rating_Logic.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[47]:
+
+
+print('{0:=<11} | {1:*^16} | {2:#>8}'.format(Nasser, Sam, Alan))
+print('{0:=<11} | {1:*^16} | {2:#>8}'.format(0,50,100))
+print('{0:=<8} | {1:*^10} | {2:#>8}'.format(a, b , c))
+
+
+# In[43]:
+
+
+a = 'broke his knee'
+b = 'scrub LoL player'
+c = 'pure and utter God'
+Nasser = 'Nasser'
+Sam = 'Sam'
+Alan = 'Alan'
+
+
+# In[44]:
+
+
+print(f"{Nasser} {a}, now he cannot play his dear old football anymore. {Sam}, the {b}, still thinks he can be the best LoL player. For we must pray to our {c} {Alan} to set things, and cure them of their afflictions.")
+
+
+# In[45]:
+
+
+del team_leader_list
+
+
+# In[ ]:
+
+
+
+
